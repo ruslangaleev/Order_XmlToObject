bfc0f617735b9e857c7a3fc95721057c4a2d2370
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-06-27 09:15:18 +0300

    Инициализация проекта

---
+++ XmlToObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlToObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
