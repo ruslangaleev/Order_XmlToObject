e171bc28667bbf8528318505c46d294b330beaf6
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-06-27 10:00:42 +0300

    Реализована задача

--- XmlToObject/Program.cs
+++ XmlToObject/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Xml;
 
 namespace XmlToObject
 {
@@ -6,7 +9,26 @@ namespace XmlToObject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // string or text from file
+            string str = GetTextFromFile("ServiceSalesHistory.txt");
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(str);
+
+            string json = JsonConvert.SerializeXmlNode(xml);
+            var model = JsonConvert.DeserializeObject<ServiceSalesHistoryModel>(json);
+
+            foreach (var entry in model.ServiceSalesHistory.sscl)
+            {
+                Console.WriteLine($"RONumber: {entry.RONumber}; VIN: {entry.VIN}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string GetTextFromFile(string fileName)
+        {
+            return File.ReadAllTextAsync(fileName).GetAwaiter().GetResult();
         }
     }
 }
